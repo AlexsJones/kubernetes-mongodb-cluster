181ea4319b41ca622ec6866424eede58565b674d
AlexsJones <tibbar@freedommail.ch>
2018-10-29 17:51:30 +0000

    updated

--- build_environment.sh
+++ build_environment.sh
@@ -5,9 +5,4 @@ echo "Building for environment ${ENV}"
 
 rm -rf deployment || true
 
-TMPFILE=$(mktemp)
-/usr/bin/openssl rand -base64 741 > $TMPFILE
-kubectl create secret generic shared-bootstrap-data --from-file=internal-auth-mongodb-keyfile=$TMPFILE --dry-run --namespace={{.namespace}} -o yaml > templates/secret.yaml
-rm ${TMPFILE}
-
 vortex --template templates --output deployment -varpath ./environments/${ENV}.yaml
